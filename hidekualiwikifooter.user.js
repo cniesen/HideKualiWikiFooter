8e1affb118454b607b83c78201819c87e9f120e5
cniesen <cniesen@users.noreply.github.com>
2014-07-23 11:14:45 -0500

    Configure automatic script updates

--- hidekualiwikifooter.user.js
+++ hidekualiwikifooter.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @id             HideKualiWikiFooter
 // @name           Hide Kuali Wiki Footer
-// @version        1.0
+// @version        1.1
 // @namespace      https://github.com/cniesen/HideKualiWikiFooter
+// @updateUrl      https://raw.githubusercontent.com/cniesen/HideKualiWikiFooter/master/hidekualiwikifooter.user.js
+// @downloadURL    https://raw.githubusercontent.com/cniesen/HideKualiWikiFooter/master/hidekualiwikifooter.user.js
 // @description    Gain more real estate by hiding confluence footer
 // @include        https://wiki.kuali.org/*
 // ==/UserScript==
